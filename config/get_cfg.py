63ead1fd3b5ae06a28942ff6b64e8d9f6447f196
lamb-does-code <alessandro.lamberti98@gmail.com>
2021-10-16 10:20:02 +0200

    refactoring model loading

--- config/get_cfg.py
+++ config/get_cfg.py
@@ -1,3 +1,7 @@
-import os 
+import os
+from utils.utils import create_model
 
 GENDER_MODEL_PATH = os.getenv('GENDER_MODEL_PATH', "./weights/model.h5")
+
+model = create_model()
+model.load_weights(GENDER_MODEL_PATH)
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 from utils.preprocess import Feature_Extractor
 from utils.utils import create_model, process_prediction, speech_to_text
 
-from config.get_cfg import GENDER_MODEL_PATH
+from config.get_cfg import GENDER_MODEL_PATH, model
 
 def get_arguments():
     parser = argparse.ArgumentParser()
@@ -15,16 +15,15 @@ def get_arguments():
     return parser.parse_args()
 
 if __name__ == '__main__':
+
     args = get_arguments()
     audio_file = args.audio_file
-
-    model = create_model()
-    model.load_weights(GENDER_MODEL_PATH)
 
     features = Feature_Extractor(audio_file, mel=True).extract().reshape(1, -1)
     male_prob = model.predict(features)
     gender, confidence = process_prediction(male_prob)
     text = speech_to_text(audio_file)
-
-    print(f"Predicted: {gender}, with confidence: {confidence:.2f}")
-    print(f"Detected text: {text}")
+    
+    print("Gender predicted: {}".format(gender))
+    print("Confidence: {:.3f}".format(confidence))
+    print("Detected text: {}".format(text))
