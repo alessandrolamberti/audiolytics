c27fe97c187a22e0efe9374792a78cb641de6f7d
lamb-does-code <alessandro.lamberti98@gmail.com>
2021-10-20 11:02:12 +0200

    refactored + logging

--- utils/utils.py
+++ utils/utils.py
@@ -1,44 +1,47 @@
 import speech_recognition as sr
 import numpy as np
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, LSTM, Dropout
-
-def create_model(vector_length=128):
-    model = Sequential()
-    model.add(Dense(256, input_shape=(vector_length,)))
-    model.add(Dropout(0.3))
-    model.add(Dense(256, activation="relu"))
-    model.add(Dropout(0.3))
-    model.add(Dense(128, activation="relu"))
-    model.add(Dropout(0.3))
-    model.add(Dense(128, activation="relu"))
-    model.add(Dropout(0.3))
-    model.add(Dense(64, activation="relu"))
-    model.add(Dropout(0.3))
-    model.add(Dense(64, activation="relu"))
-    model.add(Dropout(0.3))
-    model.add(Dense(32, activation="relu"))
-    model.add(Dense(1, activation="sigmoid"))
-    model.compile(loss="binary_crossentropy", metrics=["accuracy"], optimizer="adam")
-    return model
+import time
+from config.get_cfg import logger, model
 
 def process_prediction(prediction):
     int_to_label = {0: 'male', 1: 'female'}
 
     prob_array = np.array([prediction, 1 - prediction])[:, 0, 0]
     gender = int_to_label[np.argmax(prob_array)]
-    confidence = max(prob_array)
+    confidence = float(max(prob_array))
+    return gender, confidence
+
+def digest_features(features):
+    start = time.time()
+    features.reshape(1, -1)
+    gender, confidence = process_prediction(model.predict(features))
+    end = time.time()
+    logger.info("Feature digestion and prediction time: {}".format(end-start))
+
     return gender, confidence
 
 
-def speech_to_text(wav):
+def speech_to_text(wav, show_all=False):
     r = sr.Recognizer()
+    text = ''
+    confidence = None
+
     with sr.AudioFile(wav) as source:
         audio_data = r.record(source)
+        start = time.time()
         try:
-            text = r.recognize_google(audio_data)
+            if not show_all:
+                text = r.recognize_google(audio_data, show_all=show_all)
+            else:
+                prediction = r.recognize_google(audio_data, show_all=show_all)['alternative']
+                text = prediction[0]['transcript']
+                confidence = prediction[0]['confidence']
         except sr.UnknownValueError:
             text = "Unintelligible text."
-    return text
+        end = time.time()
+    logger.info("Time taken to recognize speech: {}".format(end - start))
+
+
+    return text, confidence
 
     
