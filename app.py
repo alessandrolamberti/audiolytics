452f24328f979b90c9ad3e5f720d93411b6747e1
lamb-does-code <alessandro.lamberti98@gmail.com>
2021-10-20 11:01:03 +0200

    improved api response

--- app.py
+++ app.py
@@ -3,43 +3,39 @@ import uvicorn
 import os
 
 from utils.preprocess import Feature_Extractor
-from utils.utils import process_prediction, speech_to_text
+from utils.utils import digest_features, speech_to_text
 
-from config.get_cfg import model
-
-def build_response(gender, confidence, text):
-    response = {
-        'gender': gender,
-        'confidence': str(confidence), 
-        'predicted text': text
-    }
-
-    return response
+from config.get_cfg import model, SHOW_ALL, logger
 
 app = FastAPI()
 
 
 @app.post("/upload/")
 async def upload_file(file: UploadFile = File(...)):
-    """
-    Upload a file to the server.
-    """
-    #check if file is a wav file, http error otherwise
+
+    response = {'success': False}
+    
     if not file.filename.endswith(".wav"):
         return {"error": "File is not a wav file"}
 
     with open(file.filename, "wb") as f:
         f.write(file.file.read())
+    
+    response['filename'] = file.filename
 
-    features = Feature_Extractor(file.filename, mel=True).extract().reshape(1, -1)
-    male_prob = model.predict(features)
-    gender, confidence = process_prediction(male_prob)
-    text = speech_to_text(file.filename)
+    features = Feature_Extractor(file.filename, mel=True).extract()
+    gender, confidence = digest_features(features)
+    response['audio prediction'] = {'gender': gender, 'confidence': confidence}
+
+    text, text_confidence = speech_to_text(file.filename, show_all=SHOW_ALL)
+    response['text prediction'] = {'transcript': text, 'confidence': text_confidence}
+
+    response['success'] = True
 
     os.remove(file.filename)
+    logger.info(f"File {file.filename} removed")
 
-    return {"filename": file.filename, 
-            "prediction": build_response(gender, confidence, text)}
+    return response
 
 if __name__ == "__main__":
     uvicorn.run(app)
